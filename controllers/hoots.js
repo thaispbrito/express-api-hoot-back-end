492c31ab289e3ffee3807909fd7886492a49bfd4
Thais Palma de Brito <britop.tech@gmail.com>
2026-01-14 14:35:28 -0800

    finish express-api-hoot-back-end

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -18,4 +18,138 @@ router.post("/", verifyToken, async (req, res) => {
     }
 });
 
+// GET /hoots
+router.get("/", verifyToken, async (req, res) => {
+    try {
+        const hoots = await Hoot.find({})
+            .populate("author")
+            .sort({ createdAt: "desc" });
+        res.status(200).json(hoots);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// GET /hoots/:hootId
+router.get('/:hootId', verifyToken, async (req, res) => {
+    try {
+        // populate author of hoot and comments
+        const hoot = await Hoot.findById(req.params.hootId).populate([
+            'author',
+            'comments.author',
+        ]);
+        res.status(200).json(hoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// PUT /hoots/:hootId
+router.put("/:hootId", verifyToken, async (req, res) => {
+    try {
+        // Find the hoot:
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        // Check permissions:
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+        // Update hoot:
+        const updatedHoot = await Hoot.findByIdAndUpdate(
+            req.params.hootId,
+            req.body,
+            { new: true }
+        );
+
+        // Append req.user to the author property:
+        updatedHoot._doc.author = req.user;
+
+        // Issue JSON response:
+        res.status(200).json(updatedHoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// DELETE /hoots/:hootId
+router.delete("/:hootId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+        const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+        res.status(200).json(deletedHoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// POST /hoots/:hootId/comments
+router.post("/:hootId/comments", verifyToken, async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const hoot = await Hoot.findById(req.params.hootId);
+        hoot.comments.push(req.body);
+        // Make sure we save the information we use the .save() method because comments is not a model
+        await hoot.save();
+
+        // Find the newly created comment:
+        const newComment = hoot.comments[hoot.comments.length - 1];
+
+        newComment._doc.author = req.user;
+
+        // Respond with the newComment:
+        res.status(201).json(newComment);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// PUT /hoots/:hootId/comments/:commentId
+router.put("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+        const comment = hoot.comments.id(req.params.commentId);
+
+        // ensures the current user is the author of the comment
+        if (comment.author.toString() !== req.user._id) {
+            return res
+                .status(403)
+                .json({ message: "You are not authorized to edit this comment" });
+        }
+
+        comment.text = req.body.text;
+        await hoot.save();
+        res.status(200).json({ message: "Comment updated successfully" });
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// DELETE /hoots/:hootId/comments/:commentId
+router.delete("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+        const comment = hoot.comments.id(req.params.commentId);
+
+        // ensures the current user is the author of the comment
+        if (comment.author.toString() !== req.user._id) {
+            return res
+                .status(403)
+                .json({ message: "You are not authorized to edit this comment" });
+        }
+
+        hoot.comments.remove({ _id: req.params.commentId });
+        await hoot.save();
+        res.status(200).json({ message: "Comment deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+
 module.exports = router;
